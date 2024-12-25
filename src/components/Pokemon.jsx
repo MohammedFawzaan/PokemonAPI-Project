36ea58277832a4367ecff2e77158052a5395bd6d
Mohammed Fawzaan <fawzaanmd31@gmail.com>
2024-12-25 15:36:52 +0530

    project-comlpeted

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -54,39 +54,40 @@ const Pokemon = () => {
     }, []);
 
     return (
-        <div className="p-5">
+        <div className="p-4">
             {/* Logo Image */}
-            <div className="flex justify-center mb-5">
-                <img src={PokeImage} alt="logo-image" className="w-50 h-32 object-contain" />
+            <div className="flex justify-center mb-4">
+                <img src={PokeImage} alt="logo-image" className="w-36 h-24 object-contain" />
             </div>
 
             {/* Search Form */}
-            <form onSubmit={handleSubmit} className="flex justify-center space-x-4 mb-5">
+            <form onSubmit={handleSubmit} className="space-y-4 mb-4">
                 <input
                     type="text"
-                    name='pokemon'
-                    id='pokemon'
-                    placeholder='Search For Pokemon'
+                    name="pokemon"
+                    id="pokemon"
+                    placeholder="Search For Pokemon"
                     value={search.pokemon}
                     onChange={handleChange}
-                    className="font-semibold p-2 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 w-64 sm:w-48"
+                    className="font-semibold p-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 w-full text-sm"
                 />
                 <input
                     type="text"
-                    placeholder='Search by Type'
-                    name='type'
-                    id='type'
+                    placeholder="Search by Type"
+                    name="type"
+                    id="type"
                     value={search.type}
                     onChange={handleChange}
-                    className="font-semibold p-2 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 w-64 sm:w-48"
+                    className="font-semibold p-3 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 w-full text-sm"
                 />
-                <button type='submit' className="bg-blue-500 text-white p-2 rounded-lg hover:bg-blue-600 transition-colors duration-300">
+                <button
+                    className="bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 transition-colors duration-300 w-full text-sm">
                     CLEAR
                 </button>
             </form>
 
             {/* Pokemon Cards Grid */}
-            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4'>
+            <div className="grid grid-cols-2 gap-3">
                 {searchData.map((pokemon) => {
                     return <Card key={pokemon.id} pokemon={pokemon} />;
                 })}
